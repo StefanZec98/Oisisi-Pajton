7a88bf7576d409cfd9f3ce23832987fb9476d922
Stefan Zec <stefanzec@hotmail.rs>
2020-02-21 20:47:06 +0100

    #Set

---
+++ Set.py
@@ -0,0 +1,84 @@
+class Set:
+
+
+    def __init__(self):
+        self.noviRecnik = {}
+
+
+    def pretragaAND(self,prviRecnik, drugiRecnik):  # presek
+        '''
+        Funkcija koja vraca recnik koji je presek prosledjenih.
+        Vrednosti se sabiraju.
+
+        Args:
+            prviRecnik(dict): Recnik za prvu rec
+            drugiRecnik(dict): Recnik za drugu rec
+        Return:
+             recnikNovi
+        '''
+
+        recnikNovi={}
+        for putanja1, value in prviRecnik.iteritems():
+            for putanja2, value2 in drugiRecnik.iteritems():
+                if putanja1 == putanja2:
+                    recnikNovi[putanja1] = value + value2
+
+        return recnikNovi
+
+
+    def pretragaOR(self,prviRecnik, drugiRecnik):  # unija
+        '''
+        Funkcija koja vraca recnik koji predsatvlja uniju
+        dva prosledjena recnika.
+
+        fajlovima koji se poklapaju se sabiraju vrednosti(prioriteti),
+        a ostali, koji se ne poklapaju se samo dodaju u recnik.
+
+        Args:
+            prviRecnik(dict): prvi recnik
+            drugiRecnik(dict): drugi recnik
+
+        Return:
+            Novi recnik
+        '''
+
+        recnikNovi={}
+
+        # prvo ubacujem one iz unije
+        for putanja1, value in prviRecnik.iteritems():
+            for putanja2, value2 in drugiRecnik.iteritems():
+                if putanja1 == putanja2:
+                    recnikNovi[putanja1] = value + value2
+
+        # zatim ubacujemo one koji su u prvoj a nisu u drugoj
+        for putanja1, value in prviRecnik.iteritems():
+            if putanja1 not in drugiRecnik:
+                recnikNovi[putanja1] = value
+
+        # zatim ubacujemo one koji su u drugoj a nisu u prvoj
+        for putanja2, value in drugiRecnik.iteritems():
+            if putanja2 not in prviRecnik:
+                recnikNovi[putanja2] = value
+
+        return recnikNovi
+
+    def pretragaNOT(self,prviRecnik, drugiRecnik):
+        '''
+        Funkcija koja vraca recnik u kojem se nalaze fajlovi
+        koji se ne nalaze u drugom recniku.
+
+        Args:
+            prviRecnik(dict): Recnik u kojem se nalaze fajlovi za prvu rec
+            drugiRecnik(dict): Recnik u kojem se nalaze fajlovi za drugu rec
+
+        Return:
+            Recnik
+        '''
+
+        recnikNovi={}
+
+        for putanja1, value in prviRecnik.iteritems():
+            if putanja1 not in drugiRecnik:
+                recnikNovi[putanja1] = value
+
+        return recnikNovi
