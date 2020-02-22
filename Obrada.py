0cb7b3caa0fbe441892592d340318932cd280b6d
Stefan Zec <stefanzec@hotmail.rs>
2020-02-22 13:47:23 +0100

    #rangiranje

--- Obrada.py
+++ Obrada.py
@@ -13,30 +13,56 @@ def pretraga_rangiranje(recnik, reci, graf):
 
 
 def pravljenjeListe_Recnika_I_Operanada(listaReci, recnik1, graf, prosledjeneReci):#listaReci, recnik1
+    '''
+        Funkcija koja menja listu tako da je svaka rec u stvari recnik
+        u kojem je kljuc putanja fajla, a vrednost broj pojavljivanja.
+        Lista je mutable tako da nije potreban return
+
+        Args:
+            listaReci(list): lista reci
+            recnik1(recnik): recnik kojem je kljuc putanja a vrednost trie
+    '''
 
     brojac = 0
     for rec in listaReci:
         if rec != 'and' and rec != 'or' and rec != 'not':
             prosledjeneReci.append(rec)
 
-            listaReci[brojac] = pretragaReci(recnik1, rec)
+            listaReci[brojac] = pretragaReci(recnik1, rec,graf) #od liste reci pravimo npr recnik1 and recnik 2 or recnik3...
         brojac += 1
 
 
 
-def pretragaReci(recnik1, rec):
+def pretragaReci(recnik1, rec,graf):
 
 
     recnikReci = {}
-
+    brojPojavljivanja = 0
+    r=0;
     for key, value in recnik1.items():
+
+        for k in (graf.vertList[key].connectedTo):
+            try:
+
+                brojPojavljivanja += int(pretraga_stablo(recnik1[k.id], rec)[1]) #brojPojavljivanja je broj pojavljivanja te reci u svim linkovima
+            except:
+                pass
+
 
         if int(pretraga_stablo(value, rec)[1]) != 0: #find_prefix(value, rec)[0] == True
 
+            rang = pretraga_stablo(value, rec)[1] + brojPojavljivanja * 0.35 + len(graf.vertList[key].connectedTo) * 0.7
+            recnikReci[key] = rang
+
+            # ovo je pretraga ali je zakomentarisana jer smo je ukombinovali preko rangiranja
+            '''     
             recnikReci[key] = int(pretraga_stablo(value, rec)[1])
             print("Naziv fajla: " + str(ispisNazivaFajla(key)) +" -> " + "broj pojavljivanja reci u fajlu ------->" + str( recnikReci[key]))
+            '''
+            r+=1;
+            print(str(r) + ". " + "Naziv fajla: " + str(ispisNazivaFajla(key)) + " -> " + "rang ------->" + str( recnikReci[key]))
 
-    return recnikReci #key: putanja,  vrednost: broj
+    return recnikReci #key: putanja,  vrednost: broj tj rang za tu rec na toj putanji
 
 
 def ispisNazivaFajla(link):
