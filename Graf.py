edd78ed94f74d43e0e1c350e27563be28cce0ce3
Stefan Zec <stefanzec@hotmail.rs>
2020-02-25 04:31:42 +0100

    #izmena_grafa

--- Graf.py
+++ Graf.py
@@ -1,56 +1,142 @@
-class Vertex3:
-    def __init__(self,key):
-        self.id = key
-        self.connectedTo = {}
 
-    def addNeighbor(self,nbr,weight=0):
-        self.connectedTo[nbr] = weight
-
-    def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
-
-    def getAllNeighbors(self):
-        return  [x.id for x in self.connectedTo]
-
-    def getConnections(self):
-        return self.connectedTo.keys()
-
-    def getId(self):
-        return self.id
-
-    def getWeight(self,nbr):
-        return self.connectedTo[nbr]
-
+# -*- coding: utf-8 -*-
 class Graph:
-    def __init__(self):
-        self.vertList = {}
-        self.numVertices = 0
+    """ Reprezentacija jednostavnog grafa"""
 
-    def addVertex(self,key):
-        self.numVertices = self.numVertices + 1
-        newVertex = Vertex3(key)
-        self.vertList[key] = newVertex
-        return newVertex
+    # ------------------------- Ugnježdena klasa Vertex -------------------------
+    class Vertex:
+        """ Struktura koja predstavlja čvor grafa."""
+        __slots__ = '_element'
 
-    def getVertex(self,n):
-        if n in self.vertList:
-            return self.vertList[n]
-        else:
-            return None
+        def __init__(self, x):
+            self._element = x
 
-    def __contains__(self,n):
-        return n in self.vertList
+        def element(self):
+            """Vraća element vezan za čvor grafa."""
+            return self._element
 
-    def addEdge(self,f,t,cost=0):
-        if f not in self.vertList:
-            nv = self.addVertex(f)
-        if t not in self.vertList:
-            nv = self.addVertex(t)
-        self.vertList[f].addNeighbor(self.vertList[t], cost)
+        def __hash__(self):  # omogućava da Vertex bude ključ mape
+            return hash(id(self))
 
-    def getVertices(self):
-        return self.vertList.keys()
+        def __str__(self):
+            return str(self._element)
 
-    def __iter__(self):
-        return iter(self.vertList.values())
+    # ------------------------- Ugnježdena klasa Edge -------------------------
+    class Edge:
+        """ Struktura koja predstavlja ivicu grafa """
+        __slots__ = '_origin', '_destination', '_element'
 
+        def __init__(self, origin, destination, element):
+            self._origin = origin
+            self._destination = destination
+            self._element = element
+
+        def endpoints(self):
+            """ Vraća torku (u,v) za čvorove u i v."""
+            return (self._origin, self._destination)
+
+        def opposite(self, v):
+            """ Vraća čvor koji se nalazi sa druge strane čvora v ove ivice."""
+            if not isinstance(v, Graph.Vertex):
+                raise TypeError('v mora biti instanca klase Vertex')
+            return self._destination if v is self._origin else self._origin
+            raise ValueError('v nije čvor ivice')
+
+        def element(self):
+            """ Vraća element vezan za ivicu"""
+            return self._element
+
+        def __hash__(self):  # omogućava da Edge bude ključ mape
+            return hash((self._origin, self._destination))
+
+        def __str__(self):
+            return '({0},{1},{2})'.format(self._origin, self._destination, self._element)
+
+    # ------------------------- Metode klase Graph -------------------------
+    def __init__(self, directed=True):
+        """ Kreira prazan graf (podrazumevana vrednost je da je neusmeren).
+
+        Ukoliko se opcioni parametar directed postavi na True, kreira se usmereni graf.
+        """
+        self._outgoing = {}
+        # ukoliko je graf usmeren, kreira se pomoćna mapa
+        self._incoming = {} if directed else self._outgoing
+
+    def _validate_vertex(self, v):
+        """ Proverava da li je v čvor(Vertex) ovog grafa."""
+        if not isinstance(v, self.Vertex):
+            raise TypeError('Očekivan je objekat klase Vertex')
+        if v not in self._outgoing:
+            raise ValueError('Vertex ne pripada ovom grafu.')
+
+    def is_directed(self):
+        """ Vraća True ako je graf usmeren; False ako je neusmeren."""
+        return self._incoming is not self._outgoing  # graf je usmeren ako se mape razlikuju
+
+    def vertex_count(self):
+        """ Vraća broj čvorova u grafu."""
+        return len(self._outgoing)
+
+    def vertices(self):
+        """ Vraća iterator nad svim čvorovima grafa."""
+        return self._outgoing.keys()
+
+    def edge_count(self):
+        """ Vraća broj ivica u grafu."""
+        total = sum(len(self._outgoing[v]) for v in self._outgoing)
+        # ukoliko je graf neusmeren, vodimo računa da ne brojimo čvorove više puta
+        return total if self.is_directed() else total // 2
+
+    def edges(self):
+        """ Vraća set svih ivica u grafu."""
+        result = set()  # pomoću seta osiguravamo da čvorove neusmerenog grafa brojimo samo jednom
+        for secondary_map in self._outgoing.values():
+            result.update(secondary_map.values())  # dodavanje ivice u rezultujući set
+        return result
+
+    def get_edge(self, u, v):
+        """ Vraća ivicu između čvorova u i v ili None ako nisu susedni."""
+        self._validate_vertex(u)
+        self._validate_vertex(v)
+        return self._outgoing[u].get(v)  # vratilo bi nulu da nisu povezana ova dva cvora
+
+    def degree(self, v, outgoing=True):
+        """ Vraća stepen čvora - broj(odlaznih) ivica iz čvora v u grafu.
+
+        Ako je graf usmeren, opcioni parametar outgoing se koristi za brojanje dolaznih ivica.
+        """
+        self._validate_vertex(v)
+        adj = self._outgoing if outgoing else self._incoming
+        return len(adj[v])
+
+    def incident_edges(self, v, outgoing=True):
+        """ Vraća sve (odlazne) ivice iz čvora v u grafu.
+
+        Ako je graf usmeren, opcioni parametar outgoing se koristi za brojanje dolaznih ivica.
+        """
+
+        self._validate_vertex(v)
+        adj = self._outgoing if outgoing else self._incoming
+        return adj[v].values()
+
+
+
+    def insert_vertex(self, x=None):
+        """ Ubacuje i vraća novi čvor (Vertex) sa elementom x"""
+        v = self.Vertex(x)
+        self._outgoing[v] = {}
+        if self.is_directed():
+            self._incoming[v] = {}  # mapa različitih vrednosti za dolazne čvorove
+        return v
+
+    def insert_edge(self, u, v, x=None):
+        """ Ubacuje i vraća novu ivicu (Edge) od u do v sa pomoćnim elementom x.
+
+        Baca ValueError ako u i v nisu čvorovi grafa.
+        Baca ValueError ako su u i v već povezani.
+        """
+        if self.get_edge(u, v) is not None:  # uključuje i proveru greške, ako nadje granu i ako nije nula baca eror
+            raise ValueError('u and v are already adjacent')
+        e = self.Edge(u, v, x)
+        self._outgoing[u][v] = e
+        self._incoming[v][u] = e
